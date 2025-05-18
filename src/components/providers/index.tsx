c822943e5ac63dbc3bca8a6cb7a37b92a9eb895c
R1013-T <takahashi.ryunosuke.job@gmail.com>
2025-05-18 18:21:32 +0900

    add storage

---
+++ src/components/providers/index.tsx
@@ -0,0 +1,15 @@
+// src/app/Providers.tsx
+'use client'
+
+import { LocationStateProvider } from '@location-state/core'
+import { NuqsAdapter } from 'nuqs/adapters/next'
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <LocationStateProvider>
+      <NuqsAdapter>
+        {children}
+      </NuqsAdapter>
+    </LocationStateProvider>
+  )
+}
