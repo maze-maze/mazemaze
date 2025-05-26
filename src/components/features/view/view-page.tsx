bfa1600f7692c9629e7362683f35ec0a2ed845eb
R1013-T <takahashi.ryunosuke.job@gmail.com>
2025-05-26 22:58:47 +0900

    fix

--- src/components/features/view/view-page.tsx
+++ src/components/features/view/view-page.tsx
@@ -1,12 +1,13 @@
+/* eslint-disable no-console */
 'use client'
 
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '🎙️/components/ui/carousel'
-import { cn } from '🎙️/lib/utils'
-import Image from 'next/image'
-import { Button } from '🎙️/components/ui/button'
-import { MessageSquareMoreIcon, Share2Icon, ThumbsDownIcon, ThumbsUpIcon } from 'lucide-react'
-import { useEffect, useState } from 'react'
 import type { CarouselApi } from '🎙️/components/ui/carousel'
+import { Button } from '🎙️/components/ui/button'
+import { Carousel, CarouselContent, CarouselItem } from '🎙️/components/ui/carousel'
+import { cn } from '🎙️/lib/utils'
+import { MessageSquareMoreIcon, Share2Icon, ThumbsDownIcon, ThumbsUpIcon } from 'lucide-react'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
 
 const items = [
   {
@@ -58,8 +59,8 @@ export default function ViewPage() {
     >
       <CarouselContent className="h-dvh">
         {Array.from({ length: 4 }).map((_, index) => (
-          <CarouselItem key={index} className='h-full w-full flex flex-col items-center gap-8 pt-20 relative'>
-            <p className='text-2xl'>{items[index].title}</p>
+          <CarouselItem key={index} className="h-full w-full flex flex-col items-center gap-8 pt-20 relative">
+            <p className="text-2xl">{items[index].title}</p>
             <div className={cn(items[index].bg, 'w-2/3 aspect-square rounded-3xl mx-auto flex items-center justify-center')}>
               <Image
                 src="/lama.png"
@@ -68,24 +69,24 @@ export default function ViewPage() {
                 height={2000}
               />
             </div>
-              <Image
-                src="/character.png"
-                alt=""
-                width={200}
-                height={2000}
+            <Image
+              src="/character.png"
+              alt=""
+              width={200}
+              height={2000}
             />
             <div className="absolute right-5 bottom-10 z-50 flex flex-col gap-4">
-              <Button size='icon' variant='outline' className='rounded-full size-13'>
-                <ThumbsUpIcon className='size-7' />
+              <Button size="icon" variant="outline" className="rounded-full size-13">
+                <ThumbsUpIcon className="size-7" />
               </Button>
-              <Button size='icon' variant='outline' className='rounded-full size-13'>
-                <ThumbsDownIcon className='size-7' />
+              <Button size="icon" variant="outline" className="rounded-full size-13">
+                <ThumbsDownIcon className="size-7" />
               </Button>
-              <Button size='icon' variant='outline' className='rounded-full size-13'>
-                <MessageSquareMoreIcon className='size-7' />
+              <Button size="icon" variant="outline" className="rounded-full size-13">
+                <MessageSquareMoreIcon className="size-7" />
               </Button>
-              <Button size='icon' variant='outline' className='rounded-full size-13'>
-                <Share2Icon className='size-7' />
+              <Button size="icon" variant="outline" className="rounded-full size-13">
+                <Share2Icon className="size-7" />
               </Button>
             </div>
           </CarouselItem>
