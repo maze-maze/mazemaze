43ff4ea5d6dc46cd073724123ec86b8f9815917a
SomaTakata <somatakata@gmail.com>
2025-06-05 20:53:58 +0900

    CompletePageコンポーネントのインデックスファイルを追加し、関連コンポーネントをエクスポート

---
+++ src/components/features/new/complete/index.ts
@@ -0,0 +1,8 @@
+// src/components/complete-page/index.ts
+export { default as InfoCard } from './info-card'
+export { default as ThemeInfo } from './theme-info'
+export { default as CharacterInfo } from './character-info'
+export { default as StructureInfo } from './structure-info'
+export { default as ScriptInfo } from './script-info'
+export { default as ActionButtons } from './action-buttons'
+export { default as CompletePage } from './complete-page'
