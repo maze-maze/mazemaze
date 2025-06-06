c56710b0d055f28c101de22f670bbece7362d15b
R1013-T <takahashi.ryunosuke.job@gmail.com>
2025-06-07 00:13:49 +0900

    add

--- src/app/(main)/[user_id]/[contents_id]/page.tsx
+++ src/app/(main)/[user_id]/[contents_id]/page.tsx
@@ -1,5 +1,7 @@
 import ContentsPage from '🎙️/components/features/contents/contents-page'
+import { Suspense } from 'react'
 
-export default async function Contents({ params }: { params: { user_id: string, contents_id: string } }) {
-  return <ContentsPage userId={params.user_id} contentsId={params.contents_id} />
+export default async function Contents({ params }: { params: Promise<{ user_id: string, contents_id: string }> }) {
+  const { user_id, contents_id } = await params
+  return <Suspense><ContentsPage userId={user_id} contentsId={contents_id} /></Suspense>
 }
